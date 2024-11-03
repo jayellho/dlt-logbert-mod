9ea6aa6c3403f6b57d0148feb0781ef8a7904aad
Jun Liang <jho88@gatech.edu>
2024-11-03 00:00:16 -0400

    edit to include creation of output directory in mounted folder for BGL.

--- scripts/download_bgl.sh
+++ scripts/download_bgl.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 file="${HOME}/.dataset/bgl/"
+output="${HOME}/.dataset/bgl/output/"
+
+# Check if the directory exists
+if [ ! -d "$output" ]; then
+  # Create the directory if it does not exist
+  mkdir -p "$output"
+  echo "Directory $output created."
+
 if [ -e $file ]
 then
   echo "$file exists"
